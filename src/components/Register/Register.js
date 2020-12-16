54eac9001419f592c7a8c7739f7c18e67b0c85e0
Puppala <puppalas@hyd-puppalas-m.ds.ad.adp.com>
2020-12-16 13:14:26 +0530

    registration page

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -6,13 +6,13 @@ import Input from "@material-ui/core/Input";
 import InputLabel from "@material-ui/core/InputLabel";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import FormControl from "@material-ui/core/FormControl";
-import TextField from "@material-ui/core/TextField";
-import Grid from "@material-ui/core/Grid";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import LocalPhoneIcon from "@material-ui/icons/LocalPhone";
 import EmailIcon from "@material-ui/icons/Email";
 import Button from "@material-ui/core/Button";
 import FormHelperText from "@material-ui/core/FormHelperText";
+import LockIcon from '@material-ui/icons/Lock';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
   margin: {
@@ -24,24 +24,86 @@ function Register() {
   const classes = useStyles();
   const history = useHistory();
   const [regDetails, setRegDetails] = useState({
-    firstName: "",
-    lastName: "",
-    phone: null,
+    name: "",
+    phone: "",
     email: "",
+    password: ""
   });
   const [error, setError] = useState({});
-  const handleRegister = () => {
-    // history.push("/home");
-    console.log("2222", regDetails);
-    for (let i in regDetails) {
-      console.log("666", i, regDetails[i], typeof regDetails[i]);
-      if (regDetails[i] == "" || regDetails[i] == null) {
-        setError(`${i} is Required`);
+
+  const validateForm = () => {
+
+    let fields = { ...regDetails };
+    let errors = {};
+    let formIsValid = true;
+
+    if (!fields["name"]) {
+      formIsValid = false;
+      errors["name"] = "*Please enter your name.";
+    }
+
+    if (typeof fields["name"] !== undefined) {
+      if (!fields["name"].match(/^[a-zA-Z ]*$/)) {
+        formIsValid = false;
+        errors["name"] = "*Please enter alphabet characters only.";
       }
     }
-    setTimeout(() => {
-      console.log("444", error);
-    }, 2000);
+
+    if (!fields["phone"]) {
+      formIsValid = false;
+      errors["phone"] = "*Please enter your mobile no.";
+    }
+
+    if (typeof fields["phone"] !== undefined) {
+      if (!fields["phone"].match(/^[0-9]{10}$/)) {
+        formIsValid = false;
+        errors["phone"] = "*Please enter valid mobile no.";
+      }
+    }
+
+    if (!fields["email"]) {
+      formIsValid = false;
+      errors["email"] = "*Please enter your email-ID.";
+    }
+
+    if (typeof fields["email"] !== undefined) {
+      //regular expression for email validation
+      var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+      if (!pattern.test(fields["email"])) {
+        formIsValid = false;
+        errors["email"] = "*Please enter valid email-ID.";
+      }
+    }
+
+    if (!fields["password"]) {
+      formIsValid = false;
+      errors["password"] = "*Please enter your password.";
+    }
+
+    if (typeof fields["password"] !== "undefined") {
+      if (!fields["password"].match(/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/)) {
+        formIsValid = false;
+        errors["password"] = "*Please enter strong password.";
+      }
+    }
+
+    setError(errors);
+    return formIsValid;
+
+  }
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      let fields = {};
+      fields["name"] = "";
+      fields["phone"] = "";
+      fields["email"] = "";
+      fields["password"] = "";
+      setRegDetails(fields);
+      alert("Form submitted");
+      history.push("/home");
+    }
   };
 
   const handleChange = (e) => {
@@ -55,71 +117,108 @@ function Register() {
     <div style={{ margin: "20px" }}>
       <Paper elevation={3}>
         <div style={{ padding: "30px" }}>
-          <h3>Registration</h3>
+          <Grid container>
+            <Grid item xs={4}>
+              <Button style={{display: "flex"}} onClick={() => history.push("/")}>Back</Button>
+            </Grid>
+            <Grid item xs={4}>
+            <h3>Registration</h3>
+            </Grid>
+            <Grid item xs={4}></Grid>    
+          </Grid>
           <div>
             <FormControl className={classes.margin}>
               <InputLabel htmlFor="input-with-icon-adornment">
-                First Name
+                Name
               </InputLabel>
               <Input
                 id="input-with-icon-adornment"
+                error={error["name"] ? true : false}
+                onChange={handleChange}
+                name="name"
                 startAdornment={
                   <InputAdornment position="start">
                     <AccountCircle />
                   </InputAdornment>
                 }
               />
+              {error["name"] && error["name"].length && (
+                <FormHelperText id="component-error-text">
+                  {error["name"]}
+                </FormHelperText>
+              )}
             </FormControl>
+
             <FormControl className={classes.margin}>
               <InputLabel htmlFor="input-with-icon-adornment">
-                Last Name
-              </InputLabel>
+                Phone Number
+            </InputLabel>
               <Input
                 id="input-with-icon-adornment"
-                error={!regDetails.lastName ? true : false}
+                type="number"
+                error={error["phone"] ? true : false}
                 onChange={handleChange}
-                name="lastName"
+                name="phone"
                 startAdornment={
                   <InputAdornment position="start">
-                    <AccountCircle />
+                    <LocalPhoneIcon />
                   </InputAdornment>
                 }
               />
-              {error.includes("lastName") && (
+              {error["phone"] && error["phone"].length && (
                 <FormHelperText id="component-error-text">
-                  LastName is required
+                  {error["phone"]}
                 </FormHelperText>
               )}
             </FormControl>
           </div>
-          <FormControl className={classes.margin}>
-            <InputLabel htmlFor="input-with-icon-adornment">
-              Phone Number
-            </InputLabel>
-            <Input
-              id="input-with-icon-adornment"
-              type="number"
-              startAdornment={
-                <InputAdornment position="start">
-                  <LocalPhoneIcon />
-                </InputAdornment>
-              }
-            />
-          </FormControl>
+
+
           <FormControl className={classes.margin}>
             <InputLabel htmlFor="input-with-icon-adornment">
               Email Address
             </InputLabel>
             <Input
               id="input-with-icon-adornment"
+              error={error["email"] ? true : false}
+              onChange={handleChange}
+              name="email"
               startAdornment={
                 <InputAdornment position="start">
                   <EmailIcon />
                 </InputAdornment>
               }
             />
-          </FormControl>{" "}
+            {error["email"] && error["email"].length && (
+              <FormHelperText id="component-error-text">
+                {error["email"]}
+              </FormHelperText>
+            )}
+          </FormControl>
+
+          <FormControl className={classes.margin}>
+            <InputLabel htmlFor="input-with-icon-adornment">
+              Password
+              </InputLabel>
+            <Input
+              id="input-with-icon-adornment"
+              error={error["password"] ? true : false}
+              onChange={handleChange}
+              name="password"
+              startAdornment={
+                <InputAdornment position="start">
+                  <LockIcon />
+                </InputAdornment>
+              }
+            />
+            {error["password"] && error["password"].length && (
+              <FormHelperText id="component-error-text">
+                {error["password"]}
+              </FormHelperText>
+            )}
+          </FormControl>
           <br />
+
           <Button
             variant="contained"
             color="primary"
